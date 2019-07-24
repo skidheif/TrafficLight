7205b786ca4bbc59b04c5b4777f56cfb2431f734
John Heifetz <skidheif@gmail.com>
2019-07-24 12:55:32 +0300

    resolve task

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum Lights {
+    case red
+    case yellow
+    case green
 }
 
+class ViewController: UIViewController {
+    
+    private let alphaParameterIsOn = 1.0
+    private let alphaParameterIsOff = 0.3
+    private var currentLight = Lights.red
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
+        startButton.layer.cornerRadius = 15
+    }
+    
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            redLight.alpha = CGFloat(alphaParameterIsOn)
+            yellowLight.alpha = CGFloat(alphaParameterIsOff)
+            greenLight.alpha = CGFloat(alphaParameterIsOff)
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = CGFloat(alphaParameterIsOff)
+            yellowLight.alpha = CGFloat(alphaParameterIsOn)
+            greenLight.alpha = CGFloat(alphaParameterIsOff)
+            currentLight = .green
+        case .green:
+            redLight.alpha = CGFloat(alphaParameterIsOff)
+            yellowLight.alpha = CGFloat(alphaParameterIsOff)
+            greenLight.alpha = CGFloat(alphaParameterIsOn)
+            currentLight = .red
+        }
+    }
+}
